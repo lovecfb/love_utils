2126c5a810c167666fa378a41215890e4c3daee3
lovecfb <lovecfb@163.com>
2018-11-19 19:11:09 +0800

    init project

---
+++ src/main/java/com/love/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.love.test;
+
+/**
+ * Created by ls on 2018/11/19.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println(" hello world ... ");
+
+
+    }
+
+
+}
